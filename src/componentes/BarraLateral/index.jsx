b3afa90f0e136817a0334139687e385e72fa7197
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-02-02 19:27:25 -0300

    término da implementação da barra lateral e importações das fontes globais

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -25,6 +25,24 @@ const BarraLateral = () => {
                     iconeAtivo="/icones/mais-vistas-ativo.png">
                         Mais vistas
                     </ItemNavegacao>
+
+                    <ItemNavegacao 
+                    iconeInativo="/icones/mais-curtidas-inativo.png"
+                    iconeAtivo="/icones/mais-curtidas-ativo.png">
+                        Mais curtidas
+                    </ItemNavegacao>
+
+                    <ItemNavegacao 
+                    iconeInativo="/icones/novas-inativo.png"
+                    iconeAtivo="/icones/novas-ativo.png">
+                        Novas
+                    </ItemNavegacao>
+
+                    <ItemNavegacao 
+                    iconeInativo="/icones/surpreenda-me-inativo.png"
+                    iconeAtivo="/icones/surpreenda-me-ativo.png">
+                        Surpreenda-me
+                    </ItemNavegacao>
                     
                 </ListaEstilizada>
             </nav>
